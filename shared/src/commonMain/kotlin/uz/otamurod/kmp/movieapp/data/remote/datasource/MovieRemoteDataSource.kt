cb7f3a4cb4d7e23e4980b07d44936a657b2b1e61
otamurod <safarovotamurod@gmail.com>
2024-05-09 17:55:32 +0500

    feat: create remote datasource

---
+++ shared/src/commonMain/kotlin/uz/otamurod/kmp/movieapp/data/remote/datasource/MovieRemoteDataSource.kt
@@ -0,0 +1,19 @@
+package uz.otamurod.kmp.movieapp.data.remote.datasource
+
+import kotlinx.coroutines.withContext
+import uz.otamurod.kmp.movieapp.data.remote.api.MovieKtorApiService
+import uz.otamurod.kmp.movieapp.data.remote.entities.MoviesResultResponse
+import uz.otamurod.kmp.movieapp.util.Dispatcher
+
+internal class MovieRemoteDataSource(
+    private val movieKtorApiService: MovieKtorApiService,
+    private val dispatcher: Dispatcher
+) {
+    suspend fun getMovies(page: Int = 1) = withContext(dispatcher.io) {
+        movieKtorApiService.getMovies(page = page)
+    }
+
+    suspend fun getMovie(movieId: Int) = withCon text(dispatcher.io) {
+        movieKtorApiService.getMovie(movieId = movieId)
+    }
+}
