77f0517d81b9853c914b09e7c7022a6c9addf3ec
otamurod <safarovotamurod@gmail.com>
2024-05-12 14:23:56 +0500

    feat: androidApp:: create MovieApplication

---
+++ androidApp/src/main/java/uz/otamurod/kmp/movieapp/android/presentation/MovieApplication.kt
@@ -0,0 +1,19 @@
+package uz.otamurod.kmp.movieapp.android.presentation
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import uz.otamurod.kmp.movieapp.android.presentation.di.appModule
+import uz.otamurod.kmp.movieapp.di.getSharedModules
+
+class MovieApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger()
+            androidContext(this@MovieApplication)
+            modules(appModule + getSharedModules())
+        }
+    }
+}
